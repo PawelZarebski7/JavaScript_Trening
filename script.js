4f3fe20eca4ea6e26b99dc6ad16a52f5d8495015
PawelZarebski7 <pawelzarebski7>
2021-04-28 23:26:53 +0200

    Operators

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1. Arithmetic operators + - / * %
+
+// 2. Assignment operators +=
+
+// 3. Increment / decrement --e e-- e++ ++e
+
+// 4. Comparison operators == === > < => <=
+
+// 5. Logical operators: && (and) || (or) !(not)
+
+// 6. Ternary operator:
+// let x = first ? first : secound
